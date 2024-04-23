d5afb8d5f9eb25e7db957fb56a42c46fe1998023
V.431_ <waelabdelal7@gmail.com>
2024-04-23 17:21:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        // Visual rep: https://i.imgur.com/RqsjwEX.png
+        // tree nodes
+        var root = new TreeNode<string>("Root");
+        var child1 = new TreeNode<string>("Child1");
+        var child2 = new TreeNode<string>("Child2");
+        var child11 = new TreeNode<string>("Child1-1");
+        var child12 = new TreeNode<string>("Child1-2");
+        var child21 = new TreeNode<string>("Child2-1");
+        var child22 = new TreeNode<string>("Child2-2");
+
+        // the tree
+        root.AddChild(child1);
+        root.AddChild(child2);
+        child1.AddChild(child11);
+        child1.AddChild(child12);
+        child2.AddChild(child21);
+        child2.AddChild(child22);
+
+        Console.WriteLine("Depth-First Traversal:");
+        foreach (var value in root.DepthFirstTraversal())
+        {
+            Console.WriteLine(value);
+        }
+
+        Console.WriteLine("\nBreadth-First Traversal:");
+        foreach (var value in root.BreadthFirstTraversal())
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ TreeNode.cs
@@ -0,0 +1,49 @@
+﻿public class TreeNode<T>
+{
+    public T Value { get; set; }
+    public List<TreeNode<T>> Children { get; set; }
+
+    public TreeNode(T value)
+    {
+        Value = value;
+        Children = new List<TreeNode<T>>();
+    }
+
+    public void AddChild(TreeNode<T> child)
+    {
+        Children.Add(child);
+    }
+
+    public IEnumerable<T> DepthFirstTraversal()
+    {
+        Stack<TreeNode<T>> stack = new Stack<TreeNode<T>>();
+        stack.Push(this); // r00t
+
+        while (stack.Count > 0)
+        {
+            var current = stack.Pop();
+            yield return current.Value;
+            for (int i = current.Children.Count - 1; i >= 0; i--) // push in reverse order
+            {
+                stack.Push(current.Children[i]);
+            }
+        }
+    }
+
+    public IEnumerable<T> BreadthFirstTraversal()
+    {
+        Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+        queue.Enqueue(this);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            yield return current.Value;
+            foreach (var child in current.Children)
+            {
+                queue.Enqueue(child);
+            }
+        }
+    }
+
+}
